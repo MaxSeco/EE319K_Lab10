f227e98bbd5417c7da4e48a4e48dc6c9fcf22994
Ngoc Dao <maxqseco@gmail.com>
2021-04-24 17:53:16 -0500

    Completed DAC.c ADC.c and Print.s

--- ADC.c
+++ ADC.c
@@ -1,30 +1,81 @@
 // ADC.c
 // Runs on TM4C123
-// Provide functions that initialize ADC0
-// Last Modified: 1/16/2021
+// Provide functions that initialize ADC0, channel 5, PD2
+
 // Student names: change this to your names or look very silly
 // Last modification date: change this to the last modification date or look very silly
 
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
 
+
+
 // ADC initialization function 
-// Input: none
+// Initialize ADC for PD2, analog channel 5
+// Input: sac sets hardware averaging
 // Output: none
-// measures from PD2, analog channel 5
-void ADC_Init(void){ 
-
-
+// Activating hardware averaging will improve SNR
+// Activating hardware averaging will slow down conversion
+// Max sample rate: <=125,000 samples/second
+// Sequencer 0 priority: 1st (highest)
+// Sequencer 1 priority: 2nd
+// Sequencer 2 priority: 3rd
+// Sequencer 3 priority: 4th (lowest)
+// SS3 triggering event: software trigger
+// SS3 1st sample source: Ain5 (PD2)
+// SS3 interrupts: flag set on completion but no interrupt requested
+void ADC_Init(uint32_t sac){ 
+	uint32_t delay;
+	
+	SYSCTL_RCGCGPIO_R |= 0x08;													// 1) Activate clock for Port D
+	while (( SYSCTL_PRGPIO_R&0x08) == 0) {};							
+	GPIO_PORTD_DIR_R &= ~0x4;														// 2) Make PD2 input
+	GPIO_PORTD_AFSEL_R |= 0x4;													// 3) Enable alternate function on PD2
+	GPIO_PORTD_DEN_R &= ~0x4;														// 4) Disable digital for PD2
+	GPIO_PORTD_AMSEL_R |= 0x4;													// 5) Enable analog functionon PD2
+	SYSCTL_RCGCADC_R |= 0x01;														// 6) Activate ADC0
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	delay = SYSCTL_RCGCADC_R;														// Extra time to stabilize
+	ADC0_PC_R = 0x01;																		// 7)Configure for 125K max samples/sec
+	ADC0_SAC_R = sac;																		// sets hardware averaging
+	ADC0_SSPRI_R = 0x0123;															// 8) Seq 3 is highest priority
+	ADC0_ACTSS_R &= ~0x0008;														// 9) Disable sample sequencer 3
+	ADC0_EMUX_R &= ~0xF000;															// 10) seq3 is software trigger
+	ADC0_SSMUX3_R = (ADC0_SSMUX3_R&0xFFFFFFF0)+5;				// 11) Ain5 (PD2)
+  ADC0_SSCTL3_R = 0x0006;															// 12) no TS0 D0, yes IE0 END0 
+																											// SSCTL specifies the mode of the ADC sample
+																											// Bits 3-0 are TS0, IE0, END0, D0 respectively
+																											// TS0 = 1 to measure temperature, TS0 = 0 to measure analog voltage
+																											// IE0 set for both interrupt and busy-wait synchronization
+																											// D0 = 1 to activate differential sampling, such as measuring difference ADC1-ADC0 
+																											// D0 will be 0
+																											// END0 will always be set when using seq3 signifying thsi sample is end of the sequence
+	ADC0_IM_R &= ~0x0008;																// 13) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;															// 14) enable sample sequencer 3
 }
+
 
 //------------ADC_In------------
 // Busy-wait Analog to digital conversion
 // Input: none
 // Output: 12-bit result of ADC conversion
-// measures from PD2, analog channel 5
 uint32_t ADC_In(void){  
-
-  return 0; // remove this, replace with real code
+	uint32_t data;			
+	ADC0_PSSI_R = 0x0008;								// 1) initiate conversatio on sequencer 3 (SS3)
+	while((ADC0_RIS_R&0x08) == 0) {};		// 2) wait for conversion. Bit 3 will be set when conversion (averaging) is done.
+		data = ADC0_SSFIFO3_R&0xFFF;			// 3) read 12-bit result
+		ADC0_ISC_R = 0x0008;							// 4) acknowledge completion
+		return data;	
 }
 
 
--- Print.s
+++ Print.s
@@ -1,5 +1,5 @@
 ; Print.s
-; Student names: change this to your names or look very silly
+; Student names: James Mahon and Ngoc Dao
 ; Last modification date: change this to the last modification date or look very silly
 ; Runs on TM4C123
 ; EE319K lab 7 device driver for any LCD
@@ -17,7 +17,9 @@
     AREA    |.text|, CODE, READONLY, ALIGN=2
     THUMB
 
-
+cnt		EQU		0
+n		EQU		4
+FP		RN		11
 
 ;-----------------------LCD_OutDec-----------------------
 ; Output a 32-bit number in unsigned decimal format
@@ -25,9 +27,41 @@
 ; Output: none
 ; Invariables: This function must not permanently modify registers R4 to R11
 LCD_OutDec
+		PUSH 	{FP, LR}
+		SUB		SP, #8					; allocate for n, cnt
+		MOV		FP, SP					; Frame Pointer = Stack Pointer
+		MOV		R2, #10					; R2 = 10 = divisor
+		STR		R0, [FP, #n]
+		MOV		R0, #0					
+		STR		R0, [FP, #cnt]			; cnt = 0
+ODloop	
+		LDR		R0, [FP, #n]			; R4 = n
+		UDIV	R3, R0, R2				; R3 = n/10, were n is 32-bit unsigned input
+		MUL		R1, R3, R2				; R1 = n/10*10
+		SUB		R1, R0, R1				; R1 = n%10
+		STR		R0, [FP, #n]			
+		PUSH	{R1}					; save value
+		LDR		R0, [FP, #cnt]			; R4 = cnt
+		ADD		R0, #1					; cnt++
+		STR		R0, [FP, #cnt]		
+		LDR		R0, [FP, #n]			
+		MOVS	R0, R3					; R0 n = n/10
+		STR		R0, [FP, #n]
+		CMP		R0, #0					; is n == 0?
+		BNE		ODloop					; if not continue
+		
+ODout	POP		{R0}					; restore into R0
+		ADD		R0, R0, #'0'			; convert ASCII
+		BL		SSD1306_OutChar			; print character
+		LDR		R0, [FP, #cnt]			; R4 = cnt
+		SUBS	R0, #1					; cnt--
+		STR		R0, [FP, #cnt]
+		CMP		R0, #0					; is cnt == 0?
+		BNE		ODout					; if not, loop
+		ADD		SP, #8					; deallocate
+		POP 	{FP, LR}
 
-
-      BX  LR
+		BX  LR
 ;* * * * * * * * End of LCD_OutDec * * * * * * * *
 
 ; -----------------------LCD _OutFix----------------------
@@ -43,8 +77,64 @@ LCD_OutDec
 ;       R0>999,  then output "*.** "
 ; Invariables: This function must not permanently modify registers R4 to R11
 LCD_OutFix
-
-     BX   LR
+		PUSH 	{FP, LR}
+		CMP		R0, #1000 			; is N >= 1000?;		
+		BHS		OutRange			; if so, input is out of range
+		
+*****************************************************************************
+; Copy paste from OutDec, changed a few lines (check comments)
+		SUB		SP, #8					; allocate for n, cnt
+		MOV		FP, SP					; Frame Pointer = Stack Pointer
+		MOV		R2, #10					; R2 = 10 = divisor
+		STR		R0, [FP, #n]
+		MOV		R0, #0					
+		STR		R0, [FP, #cnt]			; cnt = 0
+OFloop	
+		LDR		R0, [FP, #n]			; R4 = n
+		UDIV	R3, R0, R2				; R3 = n/10, were n is 32-bit unsigned input
+		MUL		R1, R3, R2				; R1 = n/10*10
+		SUB		R1, R0, R1				; R1 = n%10
+		STR		R0, [FP, #n]			
+		PUSH	{R1, R4}				; save value
+		LDR		R0, [FP, #cnt]			; R4 = cnt
+		ADD		R0, #1					; cnt++
+		STR		R0, [FP, #cnt]		
+		LDR		R0, [FP, #n]			
+		MOVS	R0, R3					; R0 n = n/10
+		STR		R0, [FP, #n]
+		LDR		R0, [FP, #cnt]			
+		CMP		R0, #3					; is cnt == 3?								--changed--
+		BNE		OFloop					; if not, loop								
+		
+OFout	POP		{R0, R4}				; restore into R0
+		ADD		R0, R0, #'0'			; convert ASCII
+		BL		SSD1306_OutChar			; print character
+		LDR		R0, [FP, #cnt]			; R4 = cnt
+		SUBS	R0, #1					; cnt--
+		STR		R0, [FP, #cnt]
+		CMP		R0, #2					; is cnt == 2?								--added--
+		BNE		skip1					; if not, skip								--added--
+		MOV		R0, #0x2E				; Output '.' character						--added--
+		BL		SSD1306_OutChar														
+		LDR		R0, [FP, #cnt]
+skip1	CMP		R0, #0					; is cnt == 0?
+		BNE		OFout					; if not continue
+		ADD		SP, #8					; deallocate
+******************************************************************************
+		B		OFexit
+		
+OutRange
+		MOV		R0, #0x2A			; * in ASCII
+		BL		SSD1306_OutChar		; print character
+		MOV		R0, #0x2E			; . in ASCII
+		BL		SSD1306_OutChar		; print character
+		MOV		R0, #0x2A			; * in ASCII
+		BL		SSD1306_OutChar		; print character
+		MOV		R0, #0x2A			; * in ASCII
+		BL		SSD1306_OutChar		; print character
+		
+OFexit	POP 	{FP, LR}
+		BX   	LR
  
      ALIGN
 ;* * * * * * * * End of LCD_OutFix * * * * * * * *
