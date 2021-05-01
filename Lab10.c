8a4a48691a79a33005e53286b6c2034c95ff638c
Ngoc Dao <maxqseco@gmail.com>
2021-05-01 16:55:45 -0500

    Collisions wiht platform working. Bug: Player teleports sometimes

--- Lab10.c
+++ Lab10.c
@@ -44,6 +44,7 @@
 //******************************
 
 #include <stdint.h>
+#include <stdlib.h>
 #include "../inc/tm4c123gh6pm.h"
 #include "../inc/CortexM.h"
 #include "SSD1306.h"
@@ -66,14 +67,19 @@
 #define PF3       (*((volatile uint32_t *)0x40025020))
 #define PB5       (*((volatile uint32_t *)0x40005080)) 
 #define PB4       (*((volatile uint32_t *)0x40005040)) 
-//*******************************************************************************************************************************
-#define NUM_ENEMIES		5												
-#define JUMP_SPEED		-14										// screen rotated 90 degrees to the right so "up" is negative x direction
-#define GRAVITY 			2											// gravity towards positive x direction
-#define MAX_BULLETS		6
-#define BULLET_SPEED	-8
-#define PLAYER_WIDTH	8
-#define PLAYER_HEIGHT	10
+//*******************************************************************************************************************************					
+#define NUM_PLATFORMS	 	5
+#define JUMP_SPEED			-10										// screen rotated 90 degrees to the right so "up" is negative x direction
+#define GRAVITY 				1											// gravity towards positive x direction
+#define MAX_BULLETS			10
+#define BULLET_SPEED		-8
+#define SCREEN_WIDTH  	126											// original jump speed -14 gravity 2
+#define	SCREEN_HEIGHT		62
+#define PLAYER_WIDTH		8
+#define PLAYER_HEIGHT		10
+#define PLATFORM_WIDTH 	10
+#define MAX_FALLING_SPEED	8
+
 typedef enum {dead, alive} status_t;
 struct sprite {
 	int32_t x; 							// x coordinate 0-127
@@ -84,41 +90,60 @@ struct sprite {
 };
 typedef struct sprite sprite_t;
 
-sprite_t Bullets[MAX_BULLETS];							
-sprite_t Player;					
+sprite_t Player;
+sprite_t Bullets[MAX_BULLETS];
+sprite_t Platforms[NUM_PLATFORMS];
 
 int NeedToDraw;
 
+// Initializes all game objects at the beginning of game
 void Init(void) {
+	// Initialize Player
 	Player.x = 116;
 	Player.y = 24;
 	Player.vx = JUMP_SPEED;									// player has initial y velocity to jump
 	Player.image = Player1;
 	Player.life = alive;
+	
+	// Initialize Platforms
+	Platforms[0].x = (SCREEN_WIDTH*3)/4;			// first platform's coordinates will be determined
+	Platforms[0].y = SCREEN_HEIGHT/2;
+	Platforms[0].image = Platform1;
+	Platforms[0].life = alive;
+	for (int i = 1; i < NUM_PLATFORMS; i++) {
+		Platforms[i].x = Platforms[i-1].x - Random()%30 - 8;     // next platform x coordinate based on the previous
+		Platforms[i].y = (Random()%(SCREEN_HEIGHT-10))+10;
+		Platforms[i].image = Platform1;
+		Platforms[i].life = alive;
+	}
 }
 
+
+// Called by SysTick to move all the objects on screen
+// Does not output to the LCD
 void Move(void) {
-	// Physics for the Player's x-direction
+	// Physics for the Player's y-direction
 	if (Player.life == alive) {
 			NeedToDraw = 1;
 			uint32_t adcData = ADC_In();
-			Player.y = 62-((62-8)*adcData/4096);	// adcData 0-4095, screen 0-63 pixels wide, player width 8 pixels. Slide pot now moves player.
+			Player.y = 62 -((62-8)*adcData/4096);	// adcData 0-4095, screen 0-63 pixels wide, player width 8 pixels. Slide pot now moves player.
 			
-		// physics for the Player's y-direction (Screen is flipped so x is used)
+		// physics for the Player's x-direction
 			if (Player.x >= 113) {
-				Player.x = 116;
+				Player.x = SCREEN_WIDTH - PLAYER_HEIGHT;
 				Player.vx = JUMP_SPEED;
-			} else {
-				Player.vx += GRAVITY;
-			}
+			} else if (Player.vx < MAX_FALLING_SPEED) {
+					Player.vx += GRAVITY;
+				}
 			Player.x += Player.vx;
 	}
+			
 
 	// Moves the bullets
 	for (int i = 0; i < MAX_BULLETS; i++) {
 		if (Bullets[i].life == alive) {
 			NeedToDraw = 1;
-			if ((Bullets[i].y > 62) || (Bullets[i].y < 10) ||(Bullets[i].x < 0) || (Bullets[i].x > 118)) {
+			if ((Bullets[i].y > 62) || (Bullets[i].y < 0) ||(Bullets[i].x < 0) || (Bullets[i].x > 123)) {
 					Bullets[i].life = dead;
 		  } else {
 				Bullets[i].x += Bullets[i].vx;	// moves Bullets based on its velocity
@@ -128,9 +153,11 @@ void Move(void) {
 	}
 }
 
+
+// Actually outputs to the LCD
 void Draw(void) {
 	SSD1306_ClearBuffer();
-	
+
 	// draws player
 	if (Player.life == alive) {
 		SSD1306_DrawBMP(Player.x, Player.y, Player.image, 0, SSD1306_INVERSE);
@@ -143,30 +170,54 @@ void Draw(void) {
 		}
 	}
 	
+	// draws platforms
+	for (int i = 0; i < NUM_PLATFORMS; i++) {
+		if (Platforms[i].life == alive) {
+			SSD1306_DrawBMP(Platforms[i].x, Platforms[i].y, Platforms[i].image, 0, SSD1306_INVERSE);
+		}
+	}
+	
 	SSD1306_OutBuffer();
 	NeedToDraw = 0;
 }
 
+// This function initalizes a bullet at the player's location
+// Inputs: x and y velocities of the projectile
+// Outputs: None
 void Fire(int vx, int vy) {
+	// searches bullet array for a dead bullet and bring it to life
 	for (int i = 0; i < MAX_BULLETS; i++) {
 		if (Bullets[i].life == dead) {
 			Bullets[i].x = Player.x;
 			Bullets[i].y = Player.y - PLAYER_WIDTH/2;
 			Bullets[i].image = Laser2;
-			
 			// To prevent bullets moving slower and being under Player
 			if (Player.vx < 0) {
 				Bullets[i].vx = vx + Player.vx; // bullet is at least as fast as the player
 			} else {
 				Bullets[i].vx = vx;
 			}
-	
 			Bullets[i].vy = vy;
 			Bullets[i].life = alive;
 			return;
 		}
 	}
-	
+}
+
+void Collisions(void) {
+	// checks for player collision with platforms
+	for (int i = 0; i < NUM_PLATFORMS; i++) {
+		if (Platforms[i].life == alive) {
+			int xDiff = abs(Player.x + PLAYER_HEIGHT - Platforms[i].x);
+			int yDiff = abs((Player.y-PLAYER_WIDTH) - (Platforms[i].y-PLATFORM_WIDTH));
+			if ((xDiff < 4) && (yDiff < 5) && Player.vx > 0) {
+				Player.image = BigExplosion0;
+				Player.vx = JUMP_SPEED;
+				return;
+			}
+		}
+	}
+	Player.image = Player1;
 }
 
 // **************SysTick_Init*********************
@@ -183,6 +234,7 @@ void SysTick_Init(uint32_t period){
 	NVIC_ST_CTRL_R = 0x00000007;
 }
 
+// Is called every 50ms to update the physics of the game, does not output to LCD
 void SysTick_Handler(void){ 
 	PF2 ^= 0x04;     // Heartbeat
 	static uint32_t lastdown = 0;				// this is to prevent rapid fire when button is held down
@@ -190,13 +242,12 @@ void SysTick_Handler(void){
 	
 	// fires if PE2 is pressed
 	if (down  == 0x04 && lastdown == 0) {
-		PF3 ^= 0x08;
 		Fire(BULLET_SPEED, 0);
 	}
 	
-	// moves everything on screen
-	Move();
 	
+	Move(); 			// moves everything on screen
+	Collisions(); // checks for collisions
 	lastdown = down;
 }
 //*******************************************************************************************************************************
@@ -230,11 +281,11 @@ int main(void){
   //TExaS_Init(&ScopeTask);       // or 3) call to activate analog scope PD2
   SSD1306_Init(SSD1306_SWITCHCAPVCC);
   SSD1306_OutClear();   
-  Random_Init(1);
   Profile_Init(); // PB5,PB4,PF3,PF2,PF1 
 	SysTick_Init(4000000); // interrupts every 50ms
-	ADC_Init(4);
-	Sound_Init();
+	Random_Init(NVIC_ST_CURRENT_R);
+	ADC_Init(4);				
+	//Sound_Init();
 	Switch_Init();
 	Init();
   //SSD1306_ClearBuffer();
@@ -247,7 +298,7 @@ int main(void){
   //SSD1306_DrawBMP(53, 55, Bunker0, 0, SSD1306_WHITE);
   //SSD1306_OutBuffer();											// 25 ms
 
-  while(1){
+  while(Player.life == alive){
     PF1 ^= 0x02;
 		if (NeedToDraw) {
 			Draw();
